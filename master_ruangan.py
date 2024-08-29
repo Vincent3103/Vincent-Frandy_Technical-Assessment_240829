48d9e275d319d360ba09d4b3cf7702fd49dd08ab
Vincent3103 <110580222+Vincent3103@users.noreply.github.com>
2024-08-29 10:44:45 +0700

    Create master_ruangan.py

---
+++ master_ruangan.py
@@ -0,0 +1,47 @@
+from odoo import models, fields, api, exceptions
+
+class MasterRuangan(models.Model):
+    _name = 'master.ruangan'
+
+    name = fields.Char(string='Nama Ruangan', required=True)
+    tipe_ruangan = fields.Selection([
+        ('kecil', 'Meeting Room Kecil'),
+        ('besar', 'Meeting Room Besar'),
+        ('aula', 'Aula')
+    ], string='Tipe Ruangan', required=True)
+    lokasi_ruangan = fields.Selection([
+        ('1a', '1A'),
+        ('1b', '1B'),
+        ('1c', '1C'),
+        ('2a', '2A'),
+        ('2b', '2B'),
+        ('2c', '2C')
+    ], string='Lokasi Ruangan', required=True)
+    foto_ruangan = fields.Binary(string='Foto Ruangan', required=True)
+    kapasitas_ruangan = fields.Integer(string='Kapasitas Ruangan', required=True)
+    keterangan = fields.Text(string='Keterangan')
+
+class PemesananRuangan(models.Model):
+    _name = 'pemesanan.ruangan'
+
+    nomor_pemesanan = fields.Char(string='Nomor Pemesanan', required=True)
+    ruangan_id = fields.Many2one('master.ruangan', string='Ruangan', required=True)
+    nama_pemesanan = fields.Char(string='Nama Pemesanan', required=True)
+    tanggal_pemesanan = fields.Date(string='Tanggal Pemesanan', required=True)
+    status_pemesanan = fields.Selection([
+        ('draft', 'Draft'),
+        ('ongoing', 'On Going'),
+        ('done', 'Done')
+    ], string='Status Pemesanan', default='draft')
+    catatan_pemesanan = fields.Text(string='Catatan Pemesanan')
+
+    @api.constrains('tanggal_pemesanan', 'ruangan_id')
+    def check_room_availability(self):
+        for record in self:
+            exist = self.search([
+                ('ruangan_id', '=', record.ruangan_id.id),
+                ('tanggal_pemesanan', '=', record.tanggal_pemesanan),
+                ('id', '!=', record.id)
+            ])
+            if exist:
+                raise exceptions.ValidationError('Ruangan sudah dipesan pada tanggal ini.')
