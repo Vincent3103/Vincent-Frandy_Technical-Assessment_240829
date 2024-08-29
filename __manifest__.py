1a2da9295f0fae6158422333cf5916bfbd2e5166
Vincent3103 <110580222+Vincent3103@users.noreply.github.com>
2024-08-29 10:50:44 +0700

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Ruangan dan Pemesanan',
+    'version': '1.0',
+    'category': 'Custom',
+    'summary': 'Modul untuk manajemen ruangan dan pemesanan',
+    'description': """
+        Modul ini mengelola ruangan dan pemesanan untuk acara atau pertemuan.
+    """,
+    'depends': ['base'],
+    'data': [
+        'views/ruangan.xml',
+        'views/pemesanan.xml',
+    ],
+    'application': True,
+}
